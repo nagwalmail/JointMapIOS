68a91c2eba0a0a8c9c3a72ce30188d19de242986
Alexey Kayryak <aleksey.kayryak@rosbank.ru>
2021-01-19 13:26:23 +0300

    Initial Commit

---
+++ JointMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JointMap
+//
+//  Created by Aleksey Kayryak on 19.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
